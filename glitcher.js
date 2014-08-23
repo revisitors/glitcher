e8d013c6baeaa4304011f150f315bcee714ea741
Lydia Katsamberis <llkats@gmail.com>
2014-08-22 22:06:21 -0700

    added pixel shifting glitch

--- glitcher.js
+++ glitcher.js
@@ -1,4 +1,5 @@
 module.exports.invertRGBA = invertRGBA
+module.exports.shifty = shifty
 
 function invertRGBA(rgba) {
   var inverted = new Buffer(rgba.length)
@@ -10,3 +11,15 @@ function invertRGBA(rgba) {
   }
   return inverted
 }
+
+function shifty(original) {
+  var shifted = new Buffer(original.length)
+  original.copy(shifted)
+  var len = shifted.length
+  var half = len / 2
+  for (var k = half, l = 0; k < len; k+= 4, l+= 4) {
+    shifted[k] = original[l]
+  }
+  return shifted
+}
+
